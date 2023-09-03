4bdb8b827dc9b1c84f892429d7afa54606f6e113
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-09-03 18:27:05 -0300

    update

--- src/cardProduto.js
+++ src/cardProduto.js
@@ -1,3 +1,4 @@
+import { adicionarAoCarrinho } from "./menuCarrinho";
 import { catalogo } from "./utilidade";
 
 export function renderizarCatalogo(){
@@ -9,9 +10,15 @@ export function renderizarCatalogo(){
                 <p class="text-sm">${produtoCatalogo.marca}</p>
                 <p class="text-sm">${produtoCatalogo.nome}</p>
                 <p class="text-sm">$${produtoCatalogo.preco}</p>
-                <button class="bg-slate-950 hover:bg-slate-700 text-slate-200"><i class="fa-solid fa-cart-plus"></i></button>
+                <button id="adicionar-${produtoCatalogo.id}" class="bg-slate-950 hover:bg-slate-700 text-slate-200"><i class="fa-solid fa-cart-plus"></i></button>
             </div>`;
     
     document.getElementById("container-produto").innerHTML += cartaoProduto;
+    }
+
+    for (const produtoCatalogo of catalogo){
+        document
+        .getElementById(`adicionar-${produtoCatalogo.id}`)
+        .addEventListener("click", () => adicionarAoCarrinho(produtoCatalogo.id));
     }
 }
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidade";
+
 function abrirCarrinho(){
     document.getElementById("carrinho").classList.remove("right-[-360px]");
     document.getElementById("carrinho").classList.add("right-[0px]");
@@ -16,18 +18,20 @@ export function inicializarCarrinho(){
     botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
 }
 
-export function adicionarAoCarrinho(){
+export function adicionarAoCarrinho(idProduto){
+    // p pode ser qualquer produto, é só uma forma de representar
+    const produto = catalogo.find((p) => p.id === idProduto );
     const containerProdutoCarrinho = document.getElementById("produto-carrinho");
     const cartaoProdutoCarrinho = `
 <article class="flex bg-slate-100 rounded-lg p-1 relative">
-    <button id="fechar-carrinha" class="absolute top-0 right-2">
+    <button id="fechar-carrinho" class="absolute top-0 right-2">
         <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800">
         </i></button>
-    <img src="./assets/img/product-1.jpg" alt="Camisa larga com bolsos" class="h-24">
+    <img src="./assets/img/${produto.imagem}" alt="Carrinho: ${produto.nome}" class="h-24">
     <div class="py-2">
-        <p class="text-slate-900">Camisa largo com bolsos</p>
+        <p class="text-slate-900">${produto.nome}</p>
         <p class="text-slate-400">Tamanho: M</p>
-        <p class="text-green-400">$70</p>
+        <p class="text-green-400">$${produto.preco}</p>
     </div>
 </article>`;
 
