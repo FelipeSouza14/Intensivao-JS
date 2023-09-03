612d9351aeb93dac7116848704d9f38acea71939
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-09-03 01:49:19 -0300

    update

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const catalogo = [
     
     for (const produtoCatalogo of catalogo){
         const cartaoProduto = 
-            `<div id="card-produto-1">
+            `<div class= 'border-solid border-2 border-sky-500 w-48 m-2' id="card-produto-${produtoCatalogo.id}">
                 <img id="estiloImagem" src="./assets/img/${produtoCatalogo.imagem}" alt="Produto 1 do magazine hashtag.">
                 <p>${produtoCatalogo.marca}</p>
                 <p>${produtoCatalogo.nome}</p>
